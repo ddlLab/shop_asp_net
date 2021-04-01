454a0d8d9c727e7b1bc8359bc81da2eb3c4a10d5
ddllab <ddl.rybchenko@gmail.com>
2021-04-01 21:46:50 +0300

    *sample

--- WebApplication1/Controllers/RegisterController.cs
+++ WebApplication1/Controllers/RegisterController.cs
@@ -3,45 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using protocol;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/register")]
     [ApiController]
     public class RegisterController : ControllerBase
     {
-        // GET: api/<RegisterController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<RegisterController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<RegisterController>
+        [Route("register-start")]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public RegisterStartAck Post([FromBody] RegisterStartReq requuest)
         {
-        }
-
-        // PUT api/<RegisterController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RegisterController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            RegisterStartAck response = new RegisterStartAck();
+            response.id = 100500;
+            response.result = RegisterStartAck.Result.OK;
+            return response;
         }
     }
 }
