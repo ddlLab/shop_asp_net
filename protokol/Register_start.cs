79ec5c898078cf499b6945c6e76fac9b8badc91b
akatenok <akaten.ru@gmail.com>
2021-04-03 18:30:23 +0300

    ^

---
+++ protokol/Register_start.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protokol
+{
+    public class RegisterStartReq
+    {
+       public bool type { get; set; }
+       public string email { get; set; }
+       public string pass { get; set; }
+       public string nick { get; set; }
+    }
+    public class RegisterStartAck
+    {
+        public enum Result
+        {            
+             EMAIL_NOT_FOUND      =  -4,
+             BAD_REQUEST          =  -3,
+             NON_UNIQUE_EMAIL     =  -2,
+             NON_UUNIQUE_NICKNAME =  -1,
+             OK                   =   0 
+        }
+        public int id { get; set; } = -1;
+        public Result result { get; set; }
+    }
+    public class RegisterFinishReq
+    {
+        public bool type { get; set; }
+        public string code { get; set; }
+    }
+    public class RegisterFinishAck
+    {
+        public int user_id { get; set; }
+        public enum Result
+        {
+            FAIL = -1,
+            OK = 0
+        }
+        public Result result { get; set; }
+    }
+    public class LoginReq
+    {
+        public bool type { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+    }
+    public class LoginAck
+    {
+        public int user_id { get; set; }// = RegisterStartAck.id;
+        public enum Result
+        {
+            FAIL_INVALID_PASS = -2,
+            FAIL_EMAIL_NOT_FOUND = -1,
+            SUCCESS = 0
+        }
+        public Result result { get; set; }
+    }
+    public class SetupCardReq
+    {
+        public bool type { get; set; }
+        public int user_id { get; set; }
+        public string card { get; set; }
+    }
+    public class SetupCardAck
+    {
+        public bool type { get; set; }
+        public enum Result
+        {
+            FAIL = -1,
+            OK = 0
+        }
+        public Result result { get; set; }
+    }
+
+}
