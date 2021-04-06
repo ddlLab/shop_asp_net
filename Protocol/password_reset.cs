90cefc023805fe0b3717514c7cdddacc7270a4ee
Shakhray28 <dima280803@gmail.com>
2021-04-06 20:30:48 +0300

    *

---
+++ Protocol/password_reset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protocol
+{
+    public class PassResetReq
+    {
+        public int type { get; set; }
+        public string new_pass { get; set; }
+    }
+
+    public class PassResetAck
+    {
+        public enum Result
+        {
+            SUCCESS_PASS_WAS_CHANGED=0,
+            FAIL_OLD_PASS_WAS_PRINTED=-1,
+            FAIL_INCORRECT_SYNTAX=-2,
+            FAIL_PASSWORD_WASNT_CHANGED=-3
+        }
+        public int type { get; set; }
+        public Result result { get; set; }
+    }
+}
