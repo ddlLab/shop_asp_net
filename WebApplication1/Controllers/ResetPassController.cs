4edd12252f6a0c4bcad24cb2dd65e6cfc912508b
akatenok <akaten.ru@gmail.com>
2021-04-06 20:26:29 +0300

    pass_reset

---
+++ WebApplication1/Controllers/ResetPassController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using protocol;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/password")]
+    [ApiController]
+    public class ResetPassController
+    {
+        [Route("reset-start")]
+        [HttpPost]
+        public ResetAck Post([FromBody] ResetReq request)
+        {
+            ResetAck response = new ResetAck();
+            response.confirm_message = "Madness";
+            response.result = ResetAck.Result.SUCCESS;
+            return response;
+        }
+
+        [Route("reset-finish")]
+        [HttpPost]
+        public ConfResetAck Post([FromBody] ConfResetReq request)
+        {
+            ConfResetAck response = new ConfResetAck();
+            response.result = ConfResetAck.Result.SUCCESS;
+            return response;
+        }
+    }
+}
