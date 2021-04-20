9d1af078e6ff96029ccedf4d52bd7ad40759d9ab
Shakhray28 <dima280803@gmail.com>
2021-04-20 20:09:53 +0300

    *

---
+++ db_queries/2.queries/query_insert_saler.cs
@@ -0,0 +1,57 @@
+﻿using db_queries._3.structures;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_queries._2.queries
+{
+    class Query_insert_saler : QueryBase
+    {
+        public eSaler saler = null;
+        public Query_insert_saler(eSaler _saler,
+                               SqlConnection _sqlConnection,
+                               fCompleter _completer)
+      : base(_sqlConnection, _completer, ExecType.NON_QUERY)
+        {
+            saler = _saler;
+        }
+        public override string SqlCommand()
+        {
+            return @"INSERT INTO Salers  (saler_email,
+                                          saler_password,
+                                          saler_nickname,
+                                          saler_phone,
+                                          saler_photo,
+                                          saler_desc,
+                                          saler_paycard,
+                                          balance,
+                                          is_blocked)
+                     VALUES(@saler_email,
+                            @saler_password,
+                            @saler_nickname,
+                            @saler_phone,
+                            @saler_photo,
+                            @saler_desc,
+                            @saler_paycard,
+                            @balance,
+                            @is_blocked)";
+        }
+        public override void PrepareParams(SqlCommand cmd)
+        {
+            cmd.Parameters.Add("@saler_email", SqlDbType.Text).Value = saler.Email;
+            cmd.Parameters.Add("@saler_password", SqlDbType.Text).Value = saler.Password;
+            cmd.Parameters.Add("@saler_nickname", SqlDbType.Text).Value = saler.Nickname;
+            cmd.Parameters.Add("@saler_phone", SqlDbType.Text).Value = saler.Phone;
+            cmd.Parameters.Add("@saler_photo", SqlDbType.Text).Value = saler.Photo;
+            cmd.Parameters.Add("@saler_desc", SqlDbType.Text).Value = saler.Desc;
+            cmd.Parameters.Add("@saler_paycard", SqlDbType.Text).Value = saler.Paycard;
+            cmd.Parameters.Add("@balance", SqlDbType.Int).Value = saler.Balance;
+            cmd.Parameters.Add("@is_blocked", SqlDbType.Bit).Value = saler.IsBlocked;
+        }
+    }
+}
+
---
+++ db_queries/2.queries/query_select_saler_by_email_nick.cs
@@ -0,0 +1,72 @@
+﻿using db_queries._3.structures;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_queries._2.queries
+{
+    class QuerySelectSalerByEmailNick : QueryBase
+    {
+        private string nick;
+        private string email;
+        public List<eSaler> salers;
+        public QuerySelectSalerByEmailNick(string _nick,
+                                            string _email,
+                                            SqlConnection _sqlConnection,
+                                            fCompleter _completer)
+        : base(_sqlConnection, _completer, ExecType.READER)
+        {
+            nick = _nick;
+            email = _email;
+        }
+        public override void PrepareParams(SqlCommand cmd)
+        {
+            cmd.Parameters.Add("@email", SqlDbType.Text).Value = email;
+            cmd.Parameters.Add("@nick", SqlDbType.Text).Value = nick;
+        }
+
+        public override string SqlCommand()
+        {
+            string sql = "SELECT * FROM Salers";
+            if (nick.Length > 0 && email.Length > 0)
+            {
+                sql += " WHERE saler_nickname=@saler_nick OR saler_email=@saler_email";
+            }
+            else if (nick.Length > 0)
+            {
+                sql += " WHERE saler_nickname=@saler_nick";
+            }
+            else if (email.Length > 0)
+            {
+                sql += " WHERE saler_email=@saler_email";
+            }
+            return sql;
+        }
+
+        public override void DataRead(DbDataReader reader)
+        {
+            if (reader.HasRows)
+            {
+                if (reader.Read())
+                {
+                    eClient client = new eClient();
+                    client.Id = (long)GetDataFromDb(reader, "saler_id", eValType.LONG);
+                    client.Email = (string)GetDataFromDb(reader, "saler_email", eValType.STRING);
+                    client.Password = (string)GetDataFromDb(reader, "saler_password", eValType.STRING);
+                    client.Nickname = (string)GetDataFromDb(reader, "saler_nickname", eValType.STRING);
+                    client.Paycard = (string)GetDataFromDb(reader, "saler_paycard", eValType.STRING);
+                    client.Phone = (string)GetDataFromDb(reader, "saler_phone", eValType.STRING);
+                    client.Photo = (string)GetDataFromDb(reader, "saler_photo", eValType.STRING);
+                    client.Desc = (string)GetDataFromDb(reader, "saler_desc", eValType.STRING);
+                    client.Balance = (int)GetDataFromDb(reader, "balance", eValType.INT);
+                    client.IsBlocked = (bool)GetDataFromDb(reader, "is_blocked", eValType.BOOL);
+                }
+            }
+        }
+    }
+}
