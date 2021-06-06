13a1e40553f4af073a1703291546189bc45db0ce
Shakhray28 <dima280803@gmail.com>
2021-06-06 18:15:33 +0300

    db_queries._2.queries

---
+++ db_queries/2.queries/query_update_pass_client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using db_queries._3.structures;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace db_queries._2.queries
+{
+    public class QueryUpdateClientPass : QueryBase
+    {
+        private string password;
+        private string email;
+      
+        public QueryUpdateClientPass(string _email,
+                                     string _password,
+                                     SqlConnection _sqlConnection,
+                                     fCompleter _completer)
+        : base(_sqlConnection, _completer, ExecType.NON_QUERY)
+        {
+            password = _password;
+            email = _email;
+  
+        }
+        public override string SqlCommand()
+        {
+            return @"UPDATE Clients
+                    SET client_password = @client_password
+                     WHERE client_email = @client_email;";
+        }
+        public override void PrepareParams(SqlCommand cmd)
+        {
+            cmd.Parameters.Add("@client_email", SqlDbType.Text).Value = email;
+            cmd.Parameters.Add("@client_password", SqlDbType.Text).Value = password;
+        }
+    }
+}
---
+++ db_queries/2.queries/query_update_pass_saler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using db_queries._3.structures;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace db_queries._2.queries
+{
+    public class QueryUpdateSalerPass : QueryBase
+    {
+        private string password;
+        private string email;
+
+        public QueryUpdateSalerPass(string _email,
+                                     string _password,
+                                     SqlConnection _sqlConnection,
+                                     fCompleter _completer)
+        : base(_sqlConnection, _completer, ExecType.NON_QUERY)
+        {
+            password = _password;
+            email = _email;
+
+        }
+        public override string SqlCommand()
+        {
+            return @"UPDATE Salers
+                    SET saler_password = @saler_password
+                     WHERE saler_email = @saler_email;";
+        }
+        public override void PrepareParams(SqlCommand cmd)
+        {
+            cmd.Parameters.Add("@saler_email", SqlDbType.Text).Value = email;
+            cmd.Parameters.Add("@saler_password", SqlDbType.Text).Value = password;
+        }
+    }
+}
